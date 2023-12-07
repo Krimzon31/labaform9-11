4c67590b9f66c78d4e2389d4a2f5c2a3a41566eb
Krimzon31 <83700861+Krimzon31@users.noreply.github.com>
2023-12-07 19:26:24 +0300

    Add files via upload

---
+++ HeshAutorization/OperatorForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using SD = System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace HeshAutorization
+{
+    public partial class OperatorForm : Form
+    {
+        public OperatorForm()
+        {
+            InitializeComponent();
+        }
+        private SqlConnection sqlConnection = new SqlConnection(@"Data Source=KRIMZON; Initial Catalog=LogPass; Integrated Security=True");
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+
+        public void openConnection()
+        {
+            if (sqlConnection.State == SD.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (sqlConnection.State == SD.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void OperatorForm_Load(object sender, EventArgs e)
+        {
+            openConnection();
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM applications", sqlConnection);
+
+            DataSet db = new DataSet();
+
+            dataAdapter.Fill(db);
+
+            dataGridView1.DataSource = db.Tables[0];
+
+            closeConnection();
+        }
+
+        private void RestoreTableButton_Click(object sender, EventArgs e)
+        {
+            openConnection();
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM applications", sqlConnection);
+
+            DataSet db = new DataSet();
+
+            dataAdapter.Fill(db);
+
+            dataGridView1.DataSource = db.Tables[0];
+
+            closeConnection();
+        }
+    }
+}
